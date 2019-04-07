60ff4b9f68b3ac18845a526e28916eb019edf34d
Raito Ochi <raito.light.write@i.softbank.jp>
2019-04-08 08:17:58 +0900

    [ST_OnlyChain] Improve
    * modify evaluation function
    * look for next two turns

--- strategy/onlyChainStrategy.cpp
+++ strategy/onlyChainStrategy.cpp
@@ -10,7 +10,7 @@ bool OnlyChainStrategy::State::operator<(const OnlyChainStrategy::State &a) cons
 OnlyChainStrategy::OnlyChainStrategy() : game(nullptr) {}
 
 string OnlyChainStrategy::getName() {
-    return "iwashiAI_v1.4";
+    return "iwashiAI_v1.5";
 }
 
 Action OnlyChainStrategy::getAction(Game &game) {
@@ -19,16 +19,30 @@ Action OnlyChainStrategy::getAction(Game &game) {
     game.player[0].fallObstacles();
 
     int bestChain = 0;
-    Action bestAction{};
-    rep(position, FIELD_WIDTH - 1) rep(rotation, 4) {
-        Field field = game.player[0].field;
-        int chain = field.dropPack(game.packs[game.turn], position, rotation);
-        if (chain > bestChain) {
-            bestChain = chain;
-            bestAction = Action::createDropPackAction(position, rotation);
+    int bestChain1 = 0, bestChain2 = 0;
+    Action bestAction{}, bestAction2{};
+    rep(pos1, FIELD_WIDTH - 1) rep(rot1, 4) {
+        Player player = game.player[0];
+        player.fallObstacles();
+        int chain1 = player.field.dropPack(game.packs[game.turn], pos1, rot1);
+        if (chain1 == -1) continue;
+        rep(pos2, FIELD_WIDTH - 1) rep(rot2, 4) {
+            Player player2 = player;
+            player2.fallObstacles();
+            int chain2 = player2.field.dropPack(game.packs[game.turn + 1], pos2, rot2);
+            if (chain2 == -1) continue;
+            if (max(chain1, chain2 - 1) > bestChain) {
+                bestChain = max(chain1, chain2 - 1);
+                bestChain1 = chain1;
+                bestChain2 = chain2;
+                bestAction = Action::createDropPackAction(pos1, rot1);
+                bestAction2 = Action::createDropPackAction(pos2, rot2);
+            }
         }
     }
-    cerr << "bestChain:" << bestChain << endl;
+    cerr << "turn:" << game.turn << endl;
+    cerr << "bestChain:" << bestChain1 << ", " << bestChain2 << endl;
+    cerr << "action:(" << bestAction.position << ", " << bestAction.rotation << "), (" << bestAction2.position << ", " << bestAction2.rotation << ")" << endl;
     if (bestChain >= 15) {
         return bestAction;
     }
@@ -78,8 +92,11 @@ Action OnlyChainStrategy::chokudaiSearch(int depth, double timeLimit) {
 long long OnlyChainStrategy::calcFieldScore(Field& field) {
     int maxChain = 0;
 
+    int maxHeight = 0;
     rep(y, FIELD_HEIGHT) rep(x, FIELD_WIDTH) {
-        if (field[y][x] == 0 || field[y][x] == OBSTACLE) continue;
+        if (field[y][x] == 0) continue;
+        maxHeight = max(maxHeight, y);
+        if (field[y][x] == OBSTACLE) continue;
         if (field[y + 1][x] == 0) continue;
         if ((x > 1 && field[y + 1][x - 1] == 0)
             || (x < FIELD_WIDTH - 1 && field[y + 1][x + 1] == 0)) {
@@ -90,5 +107,5 @@ long long OnlyChainStrategy::calcFieldScore(Field& field) {
         }
     }
 
-    return CHAIN_SCORE[maxChain];
+    return CHAIN_SCORE[maxChain] - max(0, maxHeight - 10);
 }
